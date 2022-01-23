327389557fc894dd4139b5507203c2e3a3efbfe8
Crowdbotics <team@crowdbotics.com>
2022-01-23 07:21:35 +0000

    Update models

--- backend/techcompilertest_33082/wsgi.py
+++ backend/techcompilertest_33082/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techcompilertest_33082.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techcompilertest_33082.settings")
 
 application = get_wsgi_application()
