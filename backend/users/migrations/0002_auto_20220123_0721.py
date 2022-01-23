0efa1800dd9c3fb632817b5f5be0afd14e874109
Crowdbotics <team@crowdbotics.com>
2022-01-23 07:25:28 +0000

    Update models

--- backend/users/migrations/0002_auto_20220123_0721.py
+++ backend/users/migrations/0002_auto_20220123_0721.py
@@ -6,23 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='useName',
+            model_name="user",
+            name="useName",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='name',
+            model_name="user",
+            name="name",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='password',
+            model_name="user",
+            name="password",
             field=models.TextField(blank=True, null=True),
         ),
     ]
