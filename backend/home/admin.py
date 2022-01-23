59b0dc7d04df582896d677bfe4a960e6b170514e
Crowdbotics <team@crowdbotics.com>
2022-01-23 08:16:10 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Employ
+
+admin.site.register(Employ)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+
+
+class Employ(models.Model):
+    "Generated Model"
+    name = models.TextField()
+    department = models.TextField()
+    email = models.TextField()
